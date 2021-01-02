f002ae441dd8faf4b58d7d4a0d7bf7191c3f4507
agussuparman <agus.soeparmans@gmail.com>
2021-01-02 12:05:30 +0700

    integer

---
+++ 3-integer/integer.rb
@@ -0,0 +1,10 @@
+nilai_1 = 9
+nilai_2 = 3
+
+puts nilai_1 + nilai_2
+puts nilai_1 - nilai_2
+puts nilai_1 * nilai_2
+puts nilai_1 / nilai_2
+puts nilai_1.even? # Memeriksa apakah nilai termasuk bilangan genap?
+puts nilai_2.odd? # Memeriksa apakah nilai termasuk bilangan ganjil?
+puts 2.even? # Memeriksa apakah nilai termasuk bilangan genap?
