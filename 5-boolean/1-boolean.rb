7faf7d4e015d1dd47d9dd97cc308f44bd10b35ba
agussuparman <agus.soeparmans@gmail.com>
2021-01-02 12:05:57 +0700

    boolean

---
+++ 5-boolean/1-boolean.rb
@@ -0,0 +1,21 @@
+nilai_1 = true # Cara menuliskan boolean bernilai true
+nilai_2 = false # Cara menuliskan boolean bernilai false
+
+puts nilai_1
+puts nilai_2
+
+puts "\n"
+
+# OR
+puts nilai_1 || nilai_2
+puts nilai_2 || nilai_1
+puts nilai_1 or nilai_1
+puts nilai_2 or nilai_2
+
+puts "\n"
+
+# AND
+puts nilai_1 && nilai_2
+puts nilai_2 && nilai_1
+puts nilai_1 and nilai_1
+puts nilai_2 and nilai_2
---
+++ 5-boolean/2-truthy_falsy.rb
@@ -0,0 +1,8 @@
+puts "benar/true" if 1
+puts "benar/true" if "string"
+puts "benar/true" if 9 / 3
+
+puts "\n"
+
+puts "salah/false" unless nil
+puts "salah/false" unless false
