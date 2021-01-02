124c8e68f18bd17a381ee3532c557289b6ff73c1
agussuparman <agus.soeparmans@gmail.com>
2021-01-02 12:05:38 +0700

    float

---
+++ 4-float/float.rb
@@ -0,0 +1,10 @@
+nilai_1 = 3
+nilai_2 = 9
+
+puts nilai_1 / nilai_2
+# Akan menghasilkan nilai 0 karena integer / integer hasilnya akan integer
+# Salah satu variabel harus menggunakan tipe data float
+
+nilai_1 = 3.0
+
+puts nilai_1 / nilai_2
